c07cef508a42eb4b84d356fef64f790be97733ba
N3ptuner <572188360@qq.com>
2019-12-20 20:47:12 +0800

    前端工作流

--- resources/views/default/_footer.blade.php
+++ resources/views/default/_footer.blade.php
@@ -1,21 +1,12 @@
-<style>
-    div.footer
-    {
-        text-align: center;
-        position: absolute;
-        margin: auto;
-        bottom: 0;
-        left: 0;
-        right: 0;
 
-
-    }
-</style>
-<div class="footer">
-    <img class="brand-icon" height="50" width="50" src="{{ asset('img/cat.gif') }}">
+<footer class="footer">
+    <img class="brand-icon" height="40" width="40" src="{{ asset('img/cat.gif') }}">
     <a href="/" target=_blank>
-        深水港
+        Miao~
     </a>
 
-    <a href="/about" >关于</a>
-</div>
+
+    <div class="float-right">
+        <a href="/about" >关于</a>
+    </div>
+</footer>
--- resources/views/default/default.blade.php
+++ resources/views/default/default.blade.php
@@ -2,17 +2,20 @@
 <html>
 <head>
     <title>@yield('title', '深水港')</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix("css/app.css") }}">
 </head>
 <body>
 {{--    <img src="{{ asset("img/banner.jpg") }}">--}}
     @include('default._header')
     @include('default._messages')
     @include('default._errors')
+    <div class="container">
+        <div class="offset-md-1 col-md-10">
+            @yield('content')
+            @include('default._footer')
 
-    @yield('content')
+        </div>
+    </div>
 
-
-    @include('default._footer')
 </body>
 </html>
